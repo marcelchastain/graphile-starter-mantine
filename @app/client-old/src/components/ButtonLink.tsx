a7f958e3e5e0608af5edfc5533680f4868155a48
Marcel Chastain <marcel.chastain@gmail.com>
2022-02-13 11:54:05 -0800

    Remove @app/client-old

--- @app/client-old/src/components/ButtonLink.tsx
+++
@@ -1,9 +0,0 @@
-import { Button, ButtonProps } from "@mantine/core";
-import React from "react";
-
-export function ButtonLink(props: ButtonProps<'a'>) {
-  const { href, ...rest } = props;
-  return (
-      <Button component={"a"} href={href} {...rest} />
-  );
-}
--- @app/client-old/src/components/Spin.tsx
+++
@@ -1,4 +0,0 @@
-import { Loader, LoaderProps } from "@mantine/core";
-import React, { FC } from "react";
-
-export const Spin: FC<LoaderProps> = (props) => <Loader {...props} />;
