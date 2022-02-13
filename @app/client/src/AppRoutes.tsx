e9ee0d3193c1a42d74442974ed573d8db271cf7b
Marcel Chastain <marcel.chastain@gmail.com>
2022-02-13 12:32:19 -0800

    Add Register page

--- @app/client/src/AppRoutes.tsx
+++ @app/client/src/AppRoutes.tsx
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router";
 const Home = lazy(() => import("./routes/Home"));
 const FourOhFourPage = lazy(() => import("./routes/404"));
 const Login = lazy(() => import("./routes/Login"));
+const Register = lazy(() => import("./routes/Register"));
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path={"/"} element={<Home />} />
       <Route path={"/login"} element={<Login />} />
+      <Route path={"/register"} element={<Register />} />
       <Route path={"*"} element={<FourOhFourPage />} />
     </Routes>
   );
