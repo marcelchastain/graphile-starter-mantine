d5bf6f0e2ecb1216a934467c887be3b08d5a4327
Marcel Chastain <marcel.chastain@gmail.com>
2022-02-13 12:28:50 -0800

    Remove About page

--- @app/client/src/AppRoutes.tsx
+++ @app/client/src/AppRoutes.tsx
@@ -2,7 +2,6 @@ import React, { lazy } from "react";
 import { Route, Routes } from "react-router";
 
 const Home = lazy(() => import("./routes/Home"));
-const About = lazy(() => import("./routes/About"));
 const FourOhFourPage = lazy(() => import("./routes/404"));
 const Login = lazy(() => import("./routes/Login"));
 
@@ -10,7 +9,6 @@ const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path={"/"} element={<Home />} />
-      <Route path={"/about"} element={<About />} />
       <Route path={"/login"} element={<Login />} />
       <Route path={"*"} element={<FourOhFourPage />} />
     </Routes>
